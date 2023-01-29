0d30229c122b4aa378dbd00993d2cf5891a54491
Eric L <ricklamontagne@gmail.com>
2023-01-29 14:35:03 -0500

    [feat] Use safearea component, also style the title and extract as a component

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,9 +1,37 @@
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import Title from '../components/Title'
 
 function GameScreen() {
   return (
-    <Text>Game screen!</Text>
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower</Text>
+        {/* + */}
+        {/* - */}
+      </View>
+      <View>
+        {/* LOG ROUNDS */}
+      </View>
+    </View>
   )
 }
 
 export default GameScreen
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ddd52f',
+    textAlign: 'center',
+    borderWidth: '2',
+    borderColor: '#ddd52f',
+    padding: 12,
+  }
+})
